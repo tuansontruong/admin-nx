aa2dd92254718e268552acfcec05d0513c3478a2
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2023-10-21 22:37:51 +0700

    setup jwt

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,36 @@
 import express, { Request, Response } from "express";
+// set dependencies
+const { expressjwt: jwt } = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const bodyParser = require("body-parser");
+require('dotenv').config()
 
 const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
+// Create middleware for checking the JWT
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ["RS256"],
+});
+
+app.get("/", checkJwt, (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express!");
 });
 
