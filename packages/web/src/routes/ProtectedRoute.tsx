b2b086e4cb35f1e484bfd8b0f45ff10672e93d4f
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2023-11-03 23:51:21 +0700

    add eslint

--- packages/web/src/routes/ProtectedRoute.tsx
+++ packages/web/src/routes/ProtectedRoute.tsx
@@ -1,33 +1,33 @@
-import { Navigate } from "react-router-dom";
-import { cloneElement, useEffect, useState } from "react";
-import { LoadingSpinner } from "@commons";
+import { Navigate } from 'react-router-dom';
+import { cloneElement, useEffect, useState } from 'react';
+import { LoadingSpinner } from '@commons';
 
-import { PATHS } from "./constants";
-import { validateToken } from "./ProtectedRoute.service";
+import { PATHS } from './constants';
+import { validateToken } from './ProtectedRoute.service';
 
 interface ProtectedRouteProps {
   children: React.ReactElement;
 }
 
 export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const accessToken = localStorage.getItem("access_token");
-
-  if (!accessToken) {
-    return <Navigate to={PATHS.LOGIN_PATH} />;
-  }
-
-  const [permissions, setPersmissions] = useState<{} | null>(null);
+  const [permissions, setPersmissions] = useState<object>();
 
   useEffect(() => {
     validateToken()
       .then(
-        (res) => new Promise((resolve) => setTimeout(() => resolve(res), 1500))
+        (res) => new Promise((resolve) => setTimeout(() => resolve(res), 1500)),
       )
       .then((res) => {
         if (!res) window.location.href = PATHS.LOGIN_PATH;
         else setPersmissions(res);
       });
   }, []);
+
+  const accessToken = localStorage.getItem('access_token');
+
+  if (!accessToken) {
+    return <Navigate to={PATHS.LOGIN_PATH} />;
+  }
 
   if (!permissions) {
     return <LoadingSpinner />;
